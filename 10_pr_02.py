ae157e536e0da2c53e0fc24a4e72e617da57496a
Yusuf Ahmed Khan <92802192+yusufak728@users.noreply.github.com>
2022-06-24 21:06:17 +0530

    Add files via upload

--- 10_pr_02.py
+++ 10_pr_02.py
@@ -10,13 +10,8 @@ class Calculator:
 
     def cube(self):
         print(f"The value of {self.number} cube is {self.number **3}")
-    
-    @staticmethod
-    def greet():
-        print("*******Hello there welcome to the best calculator of the world*******")
 
 a = Calculator(9)
-a.greet()
 a.square()
 a.squareRoot() 
 a.cube()
